f7a6a7b0ccee195b177de600b1d97ed91cdc5132
Bryan Clair <bryan@slu.edu>
2022-12-14 00:36:37 -0600

    Returned to this problem for a much nicer Pythonic solution

--- day13/solve.py
+++ day13/solve.py
@@ -7,74 +7,51 @@
 import sys
 sys.path.append("..")
 import aocutils
+import functools
+
 
 args = aocutils.parse_args()
 
-inputfile = open(args.file).read()
-pairs = [x.split('\n') for x in inputfile.split('\n\n')]
-
-def compare_lists(left,right):
-    for i in range(len(left)):
-        try:
-            v = compare(left[i],right[i])
-        except IndexError:
-            return -1  # right list ran out, wrong order
-        if v != 0:
-            return v
-    if len(right) > len(left):
-        return 1  # right order
-    return 0   # no decision
+inputfile = open(args.file).read().strip()
+pairs = [[eval(line) for line in pair.split('\n')] for pair in inputfile.split('\n\n')]
 
 def compare(left, right):
-    if isinstance(left, list):
-        if isinstance(right, list):
-            return compare_lists(left,right)
-        else:
-            assert(isinstance(right,int))
-            return compare_lists(left,[right])
-    else:
-        assert(isinstance(left,int))
-        if isinstance(right,list):
-            return compare_lists([left],right)
-        else:
-            assert(isinstance(right,int))
-            if (left < right):
-                return 1  # right order
-            if (left > right):
-                return -1  # wrong order
-            if (left == right):
-                return 0  # no decision
-    assert(False)
+    try:
+        return right - left
+    except TypeError:
+        pass
+    
+    if not isinstance(left, list):
+        left = [left]
+    if not isinstance(right, list):
+        right = [right]
+
+    for l, r in zip(left,right):
+        v = compare(l,r)
+        if v != 0:
+            return v
+        
+    # tied, break it based on which list is longer
+    return len(right) - len(left)
 
 good = 0
-for i in range(len(pairs)):
-    index = i+1
-    left = eval(pairs[i][0])
-    right = eval(pairs[i][1])
-    v = compare(left,right)
-    if v == 1:
+for index, pair in enumerate(pairs, start = 1):
+    left, right = pair
+    if compare(left,right) > 0:
         good += index
 
 print('part 1:',good)
 
-allvals = []
-for p in pairs:
-    allvals.append(eval(p[0]))
-    allvals.append(eval(p[1]))
-
+allvals = [p for pair in pairs for p in pair]
 allvals.append([[2]])
 allvals.append([[6]])
-
-import functools
 
 allvals.sort(key = functools.cmp_to_key(compare), reverse=True)
 
 sol = 1
-for i in range(len(allvals)):
-    if isinstance(allvals[i],list) and len(allvals[i]) == 1:
-        if isinstance(allvals[i][0],list) and len(allvals[i][0]) == 1:
-            if allvals[i][0][0] in [2,6]:
-                sol *= i+1
+for index, val in enumerate(allvals, start = 1):
+    if val == [[2]] or val == [[6]]:
+        sol *= index
 
 print('part 2:',sol)
 
